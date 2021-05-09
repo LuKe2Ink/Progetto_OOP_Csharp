4b25593a716c25d51103d2b7e7146d32b299b5bb
LeroySmith17 <leroy.fabbri@studio.unibo.it>
2021-05-10 04:14:35 +0900

    CSharp

---
+++ Leroy Fabbri/LeroyCSharp/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿
+namespace LeroyCSharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.Exit = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.BackColor = System.Drawing.SystemColors.Control;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(335, 102);
+            this.button1.Margin = new System.Windows.Forms.Padding(0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 50);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Nuovo Gioco";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Exit
+            // 
+            this.Exit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.Exit.FlatAppearance.BorderSize = 0;
+            this.Exit.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Exit.Location = new System.Drawing.Point(335, 400);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(130, 50);
+            this.Exit.TabIndex = 2;
+            this.Exit.Text = "Esci";
+            this.Exit.UseVisualStyleBackColor = true;
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(335, 255);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(130, 48);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Musica SI/NO";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::LeroyCSharp.Properties.Resources.GameBackground800x601;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Exit);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Exit;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Leroy Fabbri/LeroyCSharp/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace LeroyCSharp
+{
+    public partial class Form1 : Form
+    {
+        private bool active = true;
+        readonly SoundPlayer audio = new SoundPlayer(Properties.Resources.BeneaththeMask);
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            playaudio();
+        }
+
+        private void playaudio()
+        {
+            SoundPlayer audio = new SoundPlayer(Properties.Resources.BeneaththeMask);
+            audio.Play();
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string message = "Ancora in lavorazione";
+            string title = "ReDungeonGame";
+            _ = MessageBox.Show(message, title);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (active == true)
+            {
+                audio.Stop();
+                active = false;
+            }
+            else
+            {
+                audio.Play();
+                active = true;
+            }
+        }
+    }
+}
